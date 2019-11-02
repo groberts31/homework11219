b97dca09e3ebceeb7190cc5ef2176949a38c13ba
Greg Roberts <groberts3104@gmail.com>
2019-11-02 09:57:21 -0400

    removed unneccessary functions

--- script.js
+++ script.js
@@ -126,16 +126,3 @@ function copyToClip() {
 }
 
 
-// function generate(length) {
-//     var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-//     var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-//     var numbers = '0123456789';
-//     var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
-//     var all = uppercase + lowercase + numbers + symbols;
-//     var password = '';
-//     for (var index = 0; index < length; index++) {
-//         var character = Math.floor(Math.random() * all.length);
-//         password += all.substring(character, character + 1);
-//     }
-//     return password;
-// }
